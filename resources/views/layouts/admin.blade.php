637405d36954f746697b72eb1f88143abd4abed7
Vinicio Lopez de Leon <vlopez@reserachmobile.co>
2018-01-14 12:00:38 -0600

    modulo nuevo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -88,7 +88,11 @@
                 <i class="fa fa-dashboard"></i> <span>Escritorio</span>
               </a>
             </li>
-
+             <li id="liEscritorio">
+              <a href="{{url('home')}}">
+                <i class="fa fa-dashboard"></i> <span>Modulo nuevo</span>
+              </a>
+            </li>
             <li id="liAlmacen" class="treeview">
               <a href="#">
                 <i class="fa fa-laptop"></i>
